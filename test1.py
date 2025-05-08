562b8027b556adc36cf8250bf90616bd5d6422b6
Shaoyu <lsy1999lsy@gmail.com>
2025-05-08 07:06:48 +0200

    update

--- test1.py
+++
@@ -1,5 +0,0 @@
-from indexer import search_by_content,get_all_articles,search_articles_bool,search_articles_by_title, multi_field_search
-# search_articles_by_title('good')
-multi_field_search('good')
-# search_by_content("good")
-# get_all_articles(50)
